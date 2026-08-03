c5380d24a784c5f283c5f86275a5b6c488e6e168
Jahnavi-Thammisetty <janu.t2001@gmail.com>
2026-08-03 11:46:38 +0530

    chnages

--- src/main/java/com/chat/config/ChatGroqBean.java
+++ src/main/java/com/chat/config/ChatGroqBean.java
@@ -1,13 +1,10 @@
 package com.chat.config;
 
 import dev.langchain4j.model.openai.OpenAiChatModel;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
-import java.util.Base64;
 
 @Configuration("aiChatModelConfig")
 public class ChatGroqBean {
